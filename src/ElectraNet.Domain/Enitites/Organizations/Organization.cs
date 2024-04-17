c2a3fa581e75fc5567ad32f456752b4f992b3bc6
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-17 17:29:58 +0500

    Created Organization Entity

---
+++ src/ElectraNet.Domain/Enitites/Organizations/Organization.cs
@@ -0,0 +1,11 @@
+﻿using ElectraNet.Domain.Commons;
+
+namespace ElectraNet.Domain.Enitites.Organizations;
+
+public class Organization : Auditable
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+
+}
