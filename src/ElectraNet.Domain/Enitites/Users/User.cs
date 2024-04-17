edfc1196bbef5adf1404135ac7ce2e367263979c
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-17 17:22:56 +0500

    Created User entity

---
+++ src/ElectraNet.Domain/Enitites/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using ElectraNet.Domain.Commons;
+
+namespace ElectraNet.Domain.Enitites.Users;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Number { get; set; }
+    public long RoledId { get; set; }
+    public UserRole UserRole { get; set; }
+}
