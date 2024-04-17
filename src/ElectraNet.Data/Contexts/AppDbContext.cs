c29b33f955975abee06c1d17a330699fb3113251
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-04-17 18:53:46 +0500

    Added Repository

---
+++ src/ElectraNet.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+
+using ElectraNet.Domain.Enitites.Cables;
+using ElectraNet.Domain.Enitites.Commons;
+using ElectraNet.Domain.Enitites.Laboratories;
+using ElectraNet.Domain.Enitites.Positions;
+using ElectraNet.Domain.Enitites.ServiceRecords;
+using ElectraNet.Domain.Enitites.TransformerPoints;
+using ElectraNet.Domain.Enitites.Transformers;
+using ElectraNet.Domain.Enitites.Users;
+using ElectraNet.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectraNet;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Cable> Cables { get; set; }
+    public DbSet<Laboratory> Laboratories { get; set; }
+    public DbSet<TransformerPoint> TransformerPoints { get; set; }
+    public DbSet<UserRole> UserRole { get; set; }
+    public DbSet<Asset> Assets { get; set; }
+    public DbSet<Position> Positions { get; set; }
+    public DbSet<ServiceRecord> ServiceRecords { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<UserPermission> UserPermissions { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Organization> Organizations { get; set; }
+    public DbSet<Transformer> Transformers { get; set; }
+}
---
+++ src/ElectraNet.Data/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using ElectraNet.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ElectraNet.DataAccess.Repositories;
+
+public class Repository<T> : IRepository<T> where T : Auditable
+{
+    private readonly AppDbContext context;
+    private readonly DbSet<T> set;
+    public Repository(AppDbContext context)
+    {
+        this.context = context;
+        this.set = context.Set<T>();
+    }
+
+    public async ValueTask<T> InsertAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+    public async ValueTask<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<T> DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<T> DropAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<IEnumerable<T>> SelectAsEnumerableAsync(Expression<Func<T, bool>> expression = null,string[] includes = null,bool isTracked = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> SelectAsQueryable(Expression<Func<T, bool>> expression = null,string[] includes = null,bool isTracked = true)
+    {
+        throw new NotImplementedException();
+    }
+}
