f20617c0d513f10f5528f985820397755c796196
Diyorbek <muhammadjonovd793@gmail.com>
2024-04-17 17:46:32 +0500

    Created Transformer, TransformerPoint and Position Entities

---
+++ src/ElectraNet.Domain/Enitites/Transformers/Transformer.cs
@@ -0,0 +1,9 @@
+﻿using ElectraNet.Domain.Commons;
+
+namespace ElectraNet.Domain.Enitites.Transformers;
+
+public class Transformer : Auditable
+{
+    public string Description { get; set; }
+    public long TransformerPointId { get; set; }
+}
