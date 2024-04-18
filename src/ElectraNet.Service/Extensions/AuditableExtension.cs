9888a09fb11a61f4916445abbfe043978d719a5a
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-18 10:50:48 +0500

    Added AssetService

---
+++ src/ElectraNet.Service/Extensions/AuditableExtension.cs
@@ -0,0 +1,25 @@
+﻿using ElectraNet.Domain.Commons;
+using ElectraNet.Service.Helpers;
+
+namespace ElectraNet.Service.Extensions;
+
+public static class AuditableExtension
+{
+    public static void Create(this Auditable auditable)
+    {
+        auditable.CreatedAt = DateTime.UtcNow;
+        auditable.CreatedByUserId = HttpContextHelper.UserId;
+    }
+
+    public static void Update(this Auditable auditable)
+    {
+        auditable.UpdatedAt = DateTime.UtcNow;
+        auditable.UpdatedByUserId = HttpContextHelper.UserId;
+    }
+
+    public static void Delete(this Auditable auditable)
+    {
+        auditable.DeletedAt = DateTime.UtcNow;
+        auditable.DeletedByUserId = HttpContextHelper.UserId;
+    }
+}
