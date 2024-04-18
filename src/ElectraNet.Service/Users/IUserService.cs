8970f08d30a7c48c35a238a59d30388845cf4d88
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-18 17:00:26 +0500

    Added IUserService

--- src/ElectraNet.Service/Users/IUserService.cs
+++ src/ElectraNet.Service/Users/IUserService.cs
@@ -1,5 +1,13 @@
-﻿namespace ElectraNet.Service.Users;
+﻿
+using ElectraNet.Domain.Enitites.Users;
+
+namespace ElectraNet.Service.Users;
 
 public interface IUserService
 {
+    ValueTask<User> CreateAsync(User user);
+    ValueTask<User> UpdateAsync(long id, User user);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<User> GetByIdAsync(long id);
+    ValueTask<IEnumerable<User>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
 }
--- src/ElectraNet.Service/Users/UserService.cs
+++ src/ElectraNet.Service/Users/UserService.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElectraNet.Service.Users
+namespace ElectraNet.Service.Users;
+
+public class UserService : IUserService
 {
-    internal class UserService
+    public async ValueTask<User> CreateAsync(User user)
     {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<IEnumerable<User>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<User> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<User> UpdateAsync(long id, User user)
+    {
+        throw new NotImplementedException();
     }
 }
