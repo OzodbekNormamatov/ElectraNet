b924555b31a4b04c01716401325c878c4d9e801c
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-18 10:27:12 +0500

    Update FileHelper.cs

--- src/ElectraNet.Service/Helpers/FileHelper.cs
+++ src/ElectraNet.Service/Helpers/FileHelper.cs
@@ -1,4 +1,3 @@
-﻿using ElectraNet.DataAccess.UnitOfWorks;
 using ElectraNet.Domain.Enitites.Commons;
 using ElectraNet.Domain.Enums;
 using Microsoft.AspNetCore.Http;
@@ -7,7 +6,7 @@ namespace ElectraNet.Service.Helpers;
 
 public static class FileHelper
 {
-    public static async ValueTask<(string Path, string Name)> CreateFileAsync(IUnitOfWork unitOfWork, IFormFile file, FileType type)
+    public static async ValueTask<(string Path, string Name)> CreateFileAsync(IFormFile file, FileType type)
     {
         var directoryPath = Path.Combine(PathHelper.WebRootPath, nameof(type));
         if (!Directory.Exists(directoryPath))
@@ -20,15 +19,7 @@ public static class FileHelper
         file.CopyTo(memoryStream);
         var bytes = memoryStream.ToArray();
         await fileStream.WriteAsync(bytes);
-        var asset = new Asset
-        {
-            Path = fullPath,
-            Name = file.Name,
-            CreatedByUserId = HttpContextHelper.UserId
-        };
 
-        await unitOfWork.Assets.InsertAsync(asset);
-        await unitOfWork.SaveAsync();
         return (fullPath, file.FileName);
     }
 }
