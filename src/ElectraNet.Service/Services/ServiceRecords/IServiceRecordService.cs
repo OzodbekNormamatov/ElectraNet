318de28e51055e2aa5bed62a0e4c754f98cb0022
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-20 09:54:00 +0500

    Added ServiceRecord

---
+++ src/ElectraNet.Service/Services/ServiceRecords/IServiceRecordService.cs
@@ -0,0 +1,13 @@
+﻿using ElectraNet.Service.Configurations;
+using ElectraNet.Service.DTOs.ServiceRecords;
+
+namespace ElectraNet.Service.Services.ServiceRecords;
+
+public interface IServiceRecordService
+{
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<ServiceRecordViewModel> GetByIdAsync(long id);
+    ValueTask<ServiceRecordViewModel> CreateAsync(ServiceRecordCreateModel createModel);
+    ValueTask<ServiceRecordViewModel> UpdateAsync(long id, ServiceRecordUpdateModel updateModel);
+    ValueTask<IEnumerable<ServiceRecordViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
