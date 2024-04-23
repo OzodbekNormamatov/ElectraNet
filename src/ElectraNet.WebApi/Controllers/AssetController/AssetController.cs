1a9c80608349e5c64281830b012e066c6b2f70d9
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-23 12:55:29 +0500

    Added UserRoleController and AssetController

---
+++ src/ElectraNet.WebApi/Controllers/AssetController/AssetController.cs
@@ -0,0 +1,68 @@
+﻿using ElectraNet.Service.Configurations;
+using ElectraNet.Service.DTOs.Assets;
+using ElectraNet.Service.DTOs.UserRoles;
+using ElectraNet.Service.Services.Assets;
+using ElectraNet.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectraNet.WebApi.Controllers.AssetController;
+public class AssetController(IAssetService assetService) : BaseController
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] AssetCreateModel createModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await assetService.CreateAsync(createModel)
+        });
+    }
+
+    [HttpPut("{id:long}")]
+    public async ValueTask<IActionResult> PutAsync(long id, [FromBody] AssetUpdateModel updateModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await assetService.UpdateAsync(id, updateModel)
+        });
+    }
+
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await assetService.DeleteAsync(id)
+        });
+    }
+
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await assetService.GetByIdAsync(id)
+        });
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAsync(
+        [FromQuery] PaginationParams @params,
+        [FromQuery] Filter filter)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await assetService.GetAllAsync(@params, filter)
+        });
+    }
+}
+
