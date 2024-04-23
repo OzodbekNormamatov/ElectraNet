6b22827e8c2d5d0e4079039d42545be345ef9538
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-23 11:37:17 +0500

    Added Exception Handler

---
+++ src/ElectraNet.WebApi/Middlewares/AlreadyExistExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using ElectraNet.WebApi.Models;
+using ElectraNet.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ElectraNet.WebApi.Middlewares;
+
+public class AlreadyExistExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status409Conflict;
+        if (exception is not AlreadyExistException alreadyExistException)
+            return false;
+
+        await httpContext.Response.WriteAsJsonAsync(new Response
+        {
+            StatusCode = alreadyExistException.StatusCode,
+            Message = alreadyExistException.Message,
+        });
+
+        return true;
+    }
+}
---
+++ src/ElectraNet.WebApi/Middlewares/NotFoundExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using ElectraNet.WebApi.Models;
+using ElectraNet.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ElectraNet.WebApi.Middlewares;
+
+public class NotFoundExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+        if (exception is not NotFoundException notFoundException)
+            return false;
+
+        await httpContext.Response.WriteAsJsonAsync(new Response
+        {
+            StatusCode = notFoundException.StatusCode,
+            Message = notFoundException.Message,
+        });
+
+        return true;
+    }
+}
