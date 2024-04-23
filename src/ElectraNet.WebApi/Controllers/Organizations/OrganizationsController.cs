3f16f65902b3d422a4aec62ea035a34e077fd433
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-24 01:40:29 +0500

    Created Organization Controller

---
+++ src/ElectraNet.WebApi/Controllers/Organizations/OrganizationsController.cs
@@ -0,0 +1,73 @@
+﻿using ElectraNet.Service.Configurations;
+using ElectraNet.Service.DTOs.Cables;
+using ElectraNet.Service.DTOs.Organizations;
+using ElectraNet.Service.Services.Cables;
+using ElectraNet.Service.Services.Organizations;
+using ElectraNet.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectraNet.WebApi.Controllers.Organizations;
+
+public class OrganizationsController(IOrganizationService organizationService) : BaseController
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] OrganizationCreateModel createModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await organizationService.CreateAsync(createModel)
+        });
+    }
+
+    [HttpPut("{id:long}")]
+    public async ValueTask<IActionResult> PutAsync(long id, [FromBody] OrganizationUpdateModel updateModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await organizationService.UpdateAsync(id, updateModel)
+        });
+    }
+
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await organizationService.DeleteAsync(id)
+        });
+    }
+
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await organizationService.GetByIdAsync(id)
+        });
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAsync(
+        [FromQuery] PaginationParams @params,
+        [FromQuery] Filter filter,
+        [FromQuery] string search = null)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await organizationService.GetAllAsync(@params, filter, search)
+        });
+    }
+}
+
+
+
