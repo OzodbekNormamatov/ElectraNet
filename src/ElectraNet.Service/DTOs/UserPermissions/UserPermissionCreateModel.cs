a1fff5207ed33ce383108e2658012d88770f1e95
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-24 15:48:41 +0500

    Permission UserPermission

--- src/ElectraNet.Service/DTOs/UserPermissions/UserPermissionCreateModel.cs
+++ src/ElectraNet.Service/DTOs/UserPermissions/UserPermissionCreateModel.cs
@@ -5,7 +5,5 @@ namespace ElectraNet.Service.DTOs.UserPermissions;
 public class UserPermissionCreateModel
 {
     public long UserId { get; set; }
-    public User User { get; set; }
     public long PermissionId { get; set; }
-    public Permission Permission { get; set; }
 }
