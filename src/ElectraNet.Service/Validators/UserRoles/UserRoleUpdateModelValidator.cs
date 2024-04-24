fb8de8273f047d4abb6039c8119ef8e122cfdd8f
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-04-24 15:41:08 +0500

    Validators

---
+++ src/ElectraNet.Service/Validators/UserRoles/UserRoleUpdateModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using ElectraNet.Service.DTOs.UserRoles;
+using FluentValidation;
+
+namespace ElectraNet.WebApi.Validator.UserRoles;
+
+public class UserRoleUpdateModelValidator : AbstractValidator<UserRoleUpdateModel>
+{
+    public UserRoleUpdateModelValidator()
+    {
+        RuleFor(userRole => userRole.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage(userRole => $"{nameof(userRole.Name)} is not specified");
+    }
+}
