442d06cc22c7e46d7f25a723f869e947864cd0b4
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-24 16:14:01 +0500

    Organization, Asset

--- src/ElectraNet.Service/Mappers/MappingProfile.cs
+++ src/ElectraNet.Service/Mappers/MappingProfile.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using ElectraNet.Domain.Enitites.Commons;
+using ElectraNet.Domain.Enitites.Organizations;
+using ElectraNet.Domain.Enitites.Positions;
 using ElectraNet.Domain.Enitites.Users;
 using ElectraNet.Service.DTOs.Assets;
+using ElectraNet.Service.DTOs.Organizations;
 using ElectraNet.Service.DTOs.Permissions;
+using ElectraNet.Service.DTOs.Positions;
 using ElectraNet.Service.DTOs.UserPermissions;
 using ElectraNet.Service.DTOs.UserRoles;
 using ElectraNet.Service.DTOs.Users;
@@ -20,7 +24,6 @@ public class MappingProfile:Profile
         CreateMap<UserPermissionUpdateModel, UserPermission>().ReverseMap();
         CreateMap<UserPermission, UserPermissionViewModel>().ReverseMap();
 
-
         CreateMap<PermissionCreateModel, Permission>().ReverseMap();
         CreateMap<PermissionUpdateModel, Permission>().ReverseMap();
         CreateMap<Permission, PermissionViewModel>().ReverseMap();
@@ -34,5 +37,12 @@ public class MappingProfile:Profile
         CreateMap<UserViewModel, User>().ReverseMap();
         CreateMap<UserCreateModel, UserUpdateModel>().ReverseMap();
 
+        CreateMap<Position, PositionCreateModel>().ReverseMap();
+        CreateMap<Position, PositionUpdateModel>().ReverseMap();
+        CreateMap<PositionViewModel, Position>().ReverseMap();
+
+        CreateMap<Organization, OrganizationCreateModel>().ReverseMap();
+        CreateMap<Organization, OrganizationUpdateModel>().ReverseMap();
+        CreateMap<OrganizationViewModel, Organization>().ReverseMap();
     }
 }
--- src/ElectraNet.Service/Services/Organizations/OrganizationService.cs
+++ src/ElectraNet.Service/Services/Organizations/OrganizationService.cs
@@ -15,13 +15,13 @@ public class OrganizationService(IMapper mapper, IUnitOfWork unitOfWork) : IOrga
     {
         var existOrganization = await unitOfWork.Organizations.SelectAsync(p =>
             p.Name.ToLower() == createModel.Name.ToLower() &&
-        p.Address.ToLower() == createModel.Address.ToLower());
+        p.Address.ToLower() == createModel.Address.ToLower() && !p.IsDeleted);
 
         if (existOrganization is not null)
             throw new AlreadyExistException($"This organization is already exists | OrganizationName = {createModel.Name} Address Name = {createModel.Address}");
 
         var organization = mapper.Map<Organization>(createModel);
-        existOrganization.Create();
+        organization.Create();
         var createdOrganization = await unitOfWork.Organizations.InsertAsync(organization);
         await unitOfWork.SaveAsync();
 
@@ -34,11 +34,11 @@ public class OrganizationService(IMapper mapper, IUnitOfWork unitOfWork) : IOrga
 
         var alreadyExistOrganization = await unitOfWork.Organizations.SelectAsync(p =>
              p.Name.ToLower() == updateModel.Name.ToLower() &&
-             p.Address.ToLower() == updateModel.Address.ToLower());
+             p.Address.ToLower() == updateModel.Address.ToLower() && !p.IsDeleted);
         if (alreadyExistOrganization is not null)
             throw new AlreadyExistException($"This organization is already exists | Name = {updateModel.Name} Address Name = {updateModel.Address}");
 
-        mapper.Map(existOrganization, updateModel);
+        mapper.Map(updateModel, existOrganization);
         existOrganization.Update();
         var updateOrganization = await unitOfWork.Organizations.UpdateAsync(existOrganization);
         await unitOfWork.SaveAsync();
@@ -64,11 +64,11 @@ public class OrganizationService(IMapper mapper, IUnitOfWork unitOfWork) : IOrga
 
         if (!string.IsNullOrEmpty(search))
             organizations = organizations.Where(p =>
-             p.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
-             p.Address.Contains(search, StringComparison.OrdinalIgnoreCase));
+             p.Name.ToLower().Contains(search.ToLower()) ||
+             p.Address.ToLower().Contains(search.ToLower()));
 
         var paginateOrganizations = await organizations.ToPaginateAsQueryable(@params).ToListAsync();
-        return mapper.Map<IEnumerable<OrganizationViewModel>>(await organizations.ToPaginateAsQueryable(@params).ToListAsync());
+        return mapper.Map<IEnumerable<OrganizationViewModel>>(paginateOrganizations);
     }
 
     public async ValueTask<OrganizationViewModel> GetByIdAsync(long id)
